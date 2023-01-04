47cc1423c3e7fe97ddb8c311d9eab10f1cbae5a8
Akashi-225 <cly18mohassan@gmail.com>
2023-01-04 01:03:22 +0100

    bien crud

--- src/Controller/Admin/BienCrudController.php
+++ src/Controller/Admin/BienCrudController.php
@@ -19,14 +19,15 @@ class BienCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        yield AssociationField::new('categorie');
+        yield TextField::new('reference');
         yield TextField::new('titre');
-        yield NumberField::new('surface');
-       // yield TextField::new('postal');
-        yield BooleanField::new('status');
-       
-        yield TextField::new('ville');
+        yield TextField::new('description');
         yield TextField::new('postal');
+        yield NumberField::new('surface');
+        yield TextField::new('prix');
+        yield BooleanField::new('status');
+        yield TextField::new('ville');
+        yield AssociationField::new('categorie');
         yield ImageField::new('image')->setBasePath('uploads/images/')
         ->setUploadDir('/public/uploads/images/');
         
